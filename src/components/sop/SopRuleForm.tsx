5b5872c2360b464eb267d5e9f7bd9397db0f475a
programbisa5-project <programbisa5@gmail.com>
2026-09-08 22:12:30 +0700

    Update SopRuleForm.tsx

--- src/components/sop/SopRuleForm.tsx
+++ src/components/sop/SopRuleForm.tsx
@@ -62,8 +62,7 @@ prioritas: e.target.value })} />
 {form.isi_rule} onChange={(e) => setForm({ ...form, isi_rule:
 e.target.value })} />
 <label className="flex items-center gap-2 text-sm">
-<input type="checkbox" checked={form.status_aktif} onChange={(e)
-=> setForm({ ...form, status_aktif: e.target.checked })} />
+<input type="checkbox" checked={form.status_aktif} onChange={(e) => setForm({ ...form, status_aktif: e.target.checked })} />
 Aktif
 </label>
 </div>
