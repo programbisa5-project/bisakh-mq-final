db1cb603e71d7baf4260ea78ee6fd4223490f0cc
programbisa5-project <programbisa5@gmail.com>
2026-09-08 21:57:53 +0700

    Update MateriForm.tsx

--- src/components/materi/MateriForm.tsx
+++ src/components/materi/MateriForm.tsx
@@ -29,7 +29,7 @@ return (
 <>
 
 
-<Button variant={existing ? "secondary" : "primary"} className= {existing ? "px-2 py-1 text-xs" : ""} onClick={() => setOpen(true)}>
+<Button variant={existing ? "secondary" : "primary"} className={existing ? "px-2 py-1 text-xs" : ""} onClick={() => setOpen(true)}>
 {existing ? "Edit" : "Tambah materi"}
 </Button>
 {open ? (
